7a688945d1e5c4b706799e1fc2753dfc26cf5aa7
ewang998 <ewang998@alumni.ubc.ca>
2019-07-06 17:06:58 -0700

    added gitinore

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 var cors = require('cors')
 const app = express();
-
+app.use(express.static(path.join(__dirname, 'client/build')));
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
